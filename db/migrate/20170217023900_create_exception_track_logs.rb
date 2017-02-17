260d737ee36077c410171b7574ce11222cf6407c
Jason Lee <huacnlee@gmail.com>
2017-02-17 13:06:22 +0800

    Fix migration longtext can not work with PG

--- db/migrate/20170217023900_create_exception_track_logs.rb
+++ db/migrate/20170217023900_create_exception_track_logs.rb
@@ -2,7 +2,7 @@ class CreateExceptionTrackLogs < ActiveRecord::Migration[5.0]
   def change
     create_table :exception_tracks do |t|
       t.string :title
-      t.longtext :body
+      t.text :body
 
       t.timestamps
     end
