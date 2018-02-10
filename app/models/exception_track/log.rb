693e76f40122fba808341c3366be79be54759095
lazyb0ne <lgtwboy@gmail.com>
2018-02-10 22:11:21 +0800

    fix bug: limit title

--- app/models/exception_track/log.rb
+++ app/models/exception_track/log.rb
@@ -1,5 +1,12 @@
 module ExceptionTrack
   class Log < ActiveRecord::Base
     self.table_name = 'exception_tracks'
+    before_save :check_title
+
+    def check_title
+      max_title_len = 255
+      self.title = self.title[0, max_title-2] if self.title.size > max_title_len
+    end
+
   end
 end
