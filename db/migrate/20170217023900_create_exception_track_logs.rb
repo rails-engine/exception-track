a7bfbba267c0d92f9b3db014ce4391723d4318d2
Jason Lee <huacnlee@gmail.com>
2018-10-31 10:54:56 +0800

    Fix migration `longtext` not exist bug.

--- db/migrate/20170217023900_create_exception_track_logs.rb
+++ db/migrate/20170217023900_create_exception_track_logs.rb
@@ -4,7 +4,7 @@ class CreateExceptionTrackLogs < ActiveRecord::Migration[5.0]
   def change
     create_table :exception_tracks do |t|
       t.string :title
-      t.longtext :body
+      t.text :body, limit: 16777215
 
       t.timestamps
     end
