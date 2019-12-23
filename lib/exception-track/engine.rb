a2d9ef9ec6f121adc8af89bf6bb5fc9a68eefabd
Jason Lee <huacnlee@gmail.com>
2019-12-23 14:26:14 +0800

    Fix assets.precompile config

--- lib/exception-track/engine.rb
+++ lib/exception-track/engine.rb
@@ -3,5 +3,9 @@
 module ExceptionTrack
   class Engine < ::Rails::Engine
     isolate_namespace ExceptionTrack
+
+    initializer "exception-track.assets.precompile", group: :all do |app|
+      app.config.assets.precompile += %w( exception-track/application.css )
+    end
   end
 end
